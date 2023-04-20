c642559b4d54a10157cfa6f21b68581c84b82013
Sna1l1 <225402@student.uek.krakow.pl>
2023-04-20 17:28:20 +0200

    20.04.23 commit

--- scraper.py
+++ scraper.py
@@ -1,9 +1,11 @@
 from requests import get, codes
 from bs4 import BeautifulSoup
+import json
 # product_code = input("Please enter product code: ")
 product_code = "36991221"
 class Opinion:
-        def __init__(self, author, recomendation, stars, content, pros, cons, upvotes, downvotes, published, purchased) -> None:
+        def __init__(self,id, author, recomendation, stars, content, pros, cons, upvotes, downvotes, published, purchased) -> None:
+            self.id = id
             self.author = author
             self.recomendations = recomendation
             self.stars = stars
@@ -14,52 +16,59 @@ class Opinion:
             self.downvotes = downvotes
             self.published = published 
             self.purchased = purchased
-        
+selectors = {
+        "id":[None, "data-entry-id"],
+        "name":['span.user-post__author-name'],
+        "recomendation": ['span.user-post__author-recommendation > em'],
+        "stars": ['span.user-post__score > span.user-post__score-count'],
+        "content":['div.user-post__text'],
+        "pros":['div.review-feature__title--positives ~ div.review-feature__item', None, True],
+        "cons":['div.review-feature__title--negatives ~ div.review-feature__item', None, True],
+        "upvote":['button.vote-yes.js_product-review-vote.js_vote-yes', 'data-total-vote'],
+        "downvote":['button.vote-no.js_product-review-vote.js_vote-no','data-total-vote'],
+        "published":['span.user-post__published > time:nth-child(1)','datetime'],
+        "purchased":["span.user-post__published > time:nth-child(2)", 'datetime']
+    }
+def get_element(opinion, selector = None, attr = None, is_list = False):
+    try:
+        if is_list:
+            return [tag.text.strip() for tag in opinion.select(selector) ]
+        if not selector and attr:
+            return opinion[attr]
+        if attr:
+            return opinion.select_one(selector)[attr].strip()
+        return opinion.select_one(selector).text.strip()
+    except (AttributeError, TypeError):
+        return None
 
-
-# url = "https://www.ceneo.pl/" + product_code + "#tab=reviews"
-# url = "https://www.ceneo.pl/{}#tab=reviews".format(product_code)
 url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
-response = get(url)
-if response.status_code != codes['ok']:
-    print('Not ok')
-    exit(0)
-page = BeautifulSoup(response.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib
-
-
-try:    
-    element = page.select_one('#form_body_opinion > div > div > div.review-header__score > div > div:nth-child(1) > div.score-extend__review')
-    opinions = int(element.text.replace(' opinie', ''))
-    print(opinions)
-except:
-    exit(0)
-if opinions == 0:
-    print('no opinions')
-    exit(0)
-
 op_all = []
+while url:
+    response = get(url)
+    if response.status_code != codes['ok']:
+        print('Not ok')
+        exit(0)
+    page = BeautifulSoup(response.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib
 
-opinions = page.select('#reviews > div > div.js_product-reviews.js_reviews-hook.js_product-reviews-container > div')
-for op in  opinions:
-    name = op.select_one('span.user-post__author-name').text
-    try:
-        recomendation = op.select_one('span.user-post__author-recomendation > em').text
+    try:    
+        element = page.select_one('#form_body_opinion > div > div > div.review-header__score > div > div:nth-child(1) > div.score-extend__review')
+        opinions = int(element.text.replace(' opinie', ''))
     except:
-         recomendation = None
-    stars = op.select_one('span.user-post__score > span.user-post__score-count').text
-    content = op.select_one('div.user-post__text').text
-    pros = op.select('div.review-feature__title--positives ~ div.review-feature__item')
-    pros = [p.text.strip() for p in pros ]
-    cons = op.select('div.review-feature__title--negatives ~ div.review-feature__item')
-    cons = [p.text.strip() for p in cons ]
-    upvotes = op.select_one('button.vote-yes.js_product-review-vote.js_vote-yes[data-total-vote]').text
-    downvotes = op.select_one('button.vote-no.js_product-review-vote.js_vote-no[data-total-vote]').text
-    published = op.select_one('span.user-post__published > time:nth-child(1)')['datetime']
-    try:
-        purchased = op.select_one('span.user-post__published > time:nth-child(2)')['datetime']
+        exit(0)
+    if opinions == 0:
+        print('no opinions')
+        exit(0)
+
+    opinions = page.select('#reviews > div > div.js_product-reviews.js_reviews-hook.js_product-reviews-container > div')
+    for op in  opinions:
+        single_opinions = {}
+        for k,v in selectors.items():
+            single_opinions[k] = get_element(op, *v)
+        op_all.append(single_opinions)
+    try:    
+        url = "https://www.ceneo.pl/" + get_element(page, 'a.pagination__item.pagination__next', "href")
     except:
-         purchased = None    
-    op_all.append({
-        Opinion(name, recomendation, stars, content, pros, cons, upvotes, downvotes, published, purchased)
-    })
-input()
+        url = None
+with open('./opinions/opinions.json', "w", encoding='UTF-8') as file:
+    json.dump(op_all, file ,indent=4, ensure_ascii=False)
+print('success')
